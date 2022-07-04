398105ac92a96a4dcbe9c978447c48441d67c139
Justin Stout <midwatch@jstout.us>
2022-07-04 13:40:16 -0700

    implemented

--- mw_dry_invoke/__init__.py
+++ mw_dry_invoke/__init__.py
@@ -3,3 +3,21 @@
 __author__ = """Justin Stout"""
 __email__ = 'midwatch@jstout.us'
 __version__ = '0.1.0'
+
+from invoke import task
+from invoke.exceptions import Failure
+
+
+@task(help={'part': "major, minor, or patch/hotfix"})
+def bumpversion(ctx, part):
+    """Bump project version: major, minor, patch/hotfix
+
+    Raises:
+        Failure: part not in [major, minor, patch]
+    """
+    part = 'patch' if part == 'hotfix' else part
+
+    if part not in ['major', 'minor', 'patch']:
+        raise Failure('Not a valid part')
+
+    ctx.run(f'poetry run bump2version --no-tag {part}')
